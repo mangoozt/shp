66fc5f64e5c0b87100b1a74af9087a29b4244a06
Mark Tartagashev <mangoozt@gmail.com>
2021-11-16 14:57:42 +0300

    Use postgresql

--- portal/settings.py
+++ portal/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'data/db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': '5432',
     }
 }
 
